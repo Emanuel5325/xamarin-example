71e0f3e017af4495f60296b8e6dfbd88033a575d
Emanuel5325 <rogarcia1958@hotmail.com>
2024-01-10 17:57:54 -0300

    #8 / se agregar un mock de la respuesta de la API.

--- xamarin-example/xamarin-example/Services/Work/WorkApiClient.cs
+++ xamarin-example/xamarin-example/Services/Work/WorkApiClient.cs
@@ -1,8 +1,4 @@
 ﻿using Models.Work;
-using Newtonsoft.Json;
-using System;
-using System.Net;
-using System.Net.Http;
 
 namespace Services.Work
 {
@@ -11,46 +7,82 @@ namespace Services.Work
 
         public WorkData All(int page = 0, int pageSize = 100)
         {
-            //WorkData work = new WorkData
+            return new WorkData
+            {
+                Name = "prueba",
+                Id = 3,
+            };
+
+
+
+
+
+            //// Crea una instancia de HttpClient
+            //HttpClient client = new HttpClient();
+
+            //// Define la URL de la API
+            //string uri = $"http://localhost:5501/api/works/all?page={page}&pageSize={pageSize}";
+
+            //// Realiza la solicitud GET
+            //HttpResponseMessage response = client.GetAsync(uri).Result;
+
+            //// Verifica el código de estado de la respuesta
+            //if (response.StatusCode == HttpStatusCode.OK)
             //{
-            //    Name = "prueba",
-            //    Id = 3,
-            //};
+            //    // Decodifica la respuesta
+            //    string responseBody = response.Content.ReadAsStringAsync().Result;
 
-            //ApiRequestResult<WorkData> response = new ApiRequestResult<WorkData> { Data = work, HasError = false, };
+            //    // Imprime la respuesta
+            //    Console.WriteLine(responseBody);
 
-            //return Task.FromResult(response);
+            //    return JsonConvert.DeserializeObject<WorkData>(responseBody);
+            //}
+            //else
+            //{
+            //    // Imprime un error
+            //    Console.WriteLine("Error: {0}", response.StatusCode);
+
+            //    return null;
+            //}
 
 
 
 
-            // Crea una instancia de HttpClient
-            HttpClient client = new HttpClient();
 
-            // Define la URL de la API
-            string uri = $"http://localhost:5501/api/works/all?page={page}&pageSize={pageSize}";
 
-            // Realiza la solicitud GET
-            HttpResponseMessage response = client.GetAsync(uri).Result;
+            //// Crea una instancia de RestClient
+            //RestClient client = new RestClient("http://localhost:5501");
 
-            // Verifica el código de estado de la respuesta
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                // Decodifica la respuesta
-                string responseBody = response.Content.ReadAsStringAsync().Result;
+            //// Define la URL de la API
+            //string uri = $"/api/works/all";
 
-                // Imprime la respuesta
-                Console.WriteLine(responseBody);
+            //RestRequest request = new RestRequest(uri);
 
-                return JsonConvert.DeserializeObject<WorkData>(responseBody);
-            }
-            else
-            {
-                // Imprime un error
-                Console.WriteLine("Error: {0}", response.StatusCode);
+            //request = request.AddQueryParameter("page", page);
+            //request = request.AddQueryParameter("pageSize", pageSize);
 
-                return null;
-            }
+            //// Realiza la solicitud GET
+            //RestResponse response = client.GetAsync(request).Result;
+
+            //// Verifica el código de estado de la respuesta
+            //if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            //{
+            //    // Decodifica la respuesta
+            //    string responseBody = response.Content;
+
+            //    // Imprime la respuesta
+            //    Console.WriteLine(responseBody);
+
+            //    return JsonConvert.DeserializeObject<WorkData>(responseBody);
+            //}
+            //else
+            //{
+            //    // Imprime un error
+            //    Console.WriteLine("Error: {0}", response.StatusCode);
+
+            //    return null;
+            //}
+
         }
     }
 }
