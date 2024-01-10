30690ddc8911b9db954eb0c813818b2faa8004c9
Emanuel5325 <rogarcia1958@hotmail.com>
2024-01-10 19:23:56 -0300

    #8 / se quita código comentado.

--- xamarin-example/xamarin-example/Services/Work/WorkApiClient.cs
+++ xamarin-example/xamarin-example/Services/Work/WorkApiClient.cs
@@ -19,47 +19,13 @@ namespace Services.Work
 
 
 
-
-            //// Crea una instancia de HttpClient
-            //HttpClient client = new HttpClient();
-
-            //// Define la URL de la API
-            //string uri = $"http://localhost:5501/api/works/all?page={page}&pageSize={pageSize}";
-
-            //// Realiza la solicitud GET
-            //HttpResponseMessage response = client.GetAsync(uri).Result;
-
-            //// Verifica el código de estado de la respuesta
-            //if (response.StatusCode == HttpStatusCode.OK)
-            //{
-            //    // Decodifica la respuesta
-            //    string responseBody = response.Content.ReadAsStringAsync().Result;
-
-            //    // Imprime la respuesta
-            //    Console.WriteLine(responseBody);
-
-            //    return JsonConvert.DeserializeObject<WorkData>(responseBody);
-            //}
-            //else
-            //{
-            //    // Imprime un error
-            //    Console.WriteLine("Error: {0}", response.StatusCode);
-
-            //    return null;
-            //}
-
-
-
-
-
-
             // Crea una instancia de RestClient
             RestClient client = new RestClient("http://localhost:5501");
 
             // Define la URL de la API
             string uri = $"/api/works/all";
 
-            RestRequest request = new RestRequest(uri);
+            RestRequest request = new RestRequest(uri, Method.Get);
 
             request = request.AddQueryParameter("page", page);
             request = request.AddQueryParameter("pageSize", pageSize);
