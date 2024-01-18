60453f913a67f40b810e31465c2155ff569256cf
Emanuel5325 <rogarcia1958@hotmail.com>
2024-01-18 17:23:04 -0300

    #19 / se agrega pin en google maps.

--- Views/MapPage.xaml.cs
+++ Views/MapPage.xaml.cs
@@ -1,5 +1,6 @@
 using MauiExample.ViewModels;
 using Map = Microsoft.Maui.Controls.Maps.Map;
+using Pin = Microsoft.Maui.Controls.Maps.Pin;
 
 namespace MauiExample.Views
 {
@@ -12,6 +13,11 @@ namespace MauiExample.Views
             this.BindingContext = new MapViewModel();
 
             var map = new Map();
+            map.Pins.Add(new Pin()
+            {
+                Label = "prueba pin",
+                Location = new Location(20.737773, -156.333113),
+            });
             this.Content = map;
         }
     }
