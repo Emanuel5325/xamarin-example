5d371d830ba0329ce7907d96dbd57636bcb9ecf5
Emanuel5325 <rogarcia1958@hotmail.com>
2024-01-22 21:37:15 -0300

    #19 / se agregan implementaciones de BaseUrl en IOS y windows.

---
+++ Platforms/Windows/CustomRender/BaseUrl.cs
@@ -0,0 +1,11 @@
+﻿using MauiExample.CustomRender;
+using MauiExample.Platforms.Windows.CustomRender;
+
+[assembly: Dependency(typeof(BaseUrl))]
+namespace MauiExample.Platforms.Windows.CustomRender
+{
+    public class BaseUrl : IBaseUrl
+    {
+        public string Get() => "ms-appx-web:///";
+    }
+}
---
+++ Platforms/iOS/CustomRender/BaseUrl.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using MauiExample.CustomRender;
+
+[assembly: Dependency(typeof(MauiExample.Platforms.iOS.CustomRender.BaseUrl))]
+namespace MauiExample.Platforms.iOS.CustomRender
+{
+    public class BaseUrl : IBaseUrl
+    {
+        public string Get() => NSBundle.MainBundle.BundlePath;
+
+    }
+}
