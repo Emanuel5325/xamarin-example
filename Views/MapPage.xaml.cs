557e397f863db0d7d2a0d4a10377787f6c04138e
Emanuel5325 <rogarcia1958@hotmail.com>
2024-01-24 11:56:07 -0300

    #19 / se quitan comentarios y se agrega centrado de mapa.

--- Views/MapPage.xaml.cs
+++ Views/MapPage.xaml.cs
@@ -10,21 +10,17 @@ namespace MauiExample.Views
             InitializeComponent();
 
             this.BindingContext = new MapViewModel();
-            _ = CargarMapaAsync();
+            _ = LoadMapAsync();
 
         }
 
-        public async Task CargarMapaAsync()
+        public async Task LoadMapAsync()
         {
             var baseUrl = new BaseUrl();
             var source = new HtmlWebViewSource
             {
                 BaseUrl = baseUrl.Get()
             };
-            //var assembly = typeof(MainPage).GetTypeInfo().Assembly;
-            //_ = assembly.GetManifestResourceNames();
-
-            //var stream = assembly.GetManifestResourceStream("LeafletMap.index.html");
             var stream = await FileSystem.OpenAppPackageFileAsync("index.html");
 
             StreamReader reader = null;
@@ -52,20 +48,15 @@ namespace MauiExample.Views
 
         public void SetItemMuestra()
         {
-            //-2.14003,-79.9312967 marker
             newMarker("-2.14003", "-79.9312967", "Soy un marker");
 
-            //-2.1407283,-79.9286524 circulo
             newCircle("-2.1407283", "-79.9286524", "red", "#07", 0.5, 50);
 
-            //-2.14003,-79.9312967 / -2.1407283,-79.9286524 Linea
             newLine("-2.14003", "-79.9312967", "-2.1407283", "-79.9286524", "blue");
 
-            //mostrar objetos
             Show();
 
-            //centrar el mapa
-            //CentrarMapa("10.6222200", "-66.5735300");
+            CentrarMapa("-2.14003", "-79.9312967");
         }
         public void newMarker(string lat, string lon, string texto = "")
         {
@@ -97,6 +88,14 @@ namespace MauiExample.Views
         }
         public void Show() => this.webView.Eval(@"show()");
 
-
+        public void CentrarMapa(string lat, string lon)
+        {
+            if (string.IsNullOrEmpty(lat) || string.IsNullOrEmpty(lon))
+            {
+                Console.Write("Verifique los campos lat lon");
+                return;
+            }
+            this.webView.Eval(string.Format("centerMap({0},{1})", "\"" + lat + "\"", "\"" + lon + "\""));
+        }
     }
 }
