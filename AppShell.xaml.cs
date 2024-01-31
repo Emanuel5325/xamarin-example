efe9899d294a4f4f55aef52f33bd18f2f3d8176d
Emanuel5325 <rogarcia1958@hotmail.com>
2024-01-31 11:32:14 -0300

    #28 / se agregan route registrations.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -9,6 +9,11 @@ namespace MauiExample
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(AboutPage), typeof(AboutPage));
+            Routing.RegisterRoute(nameof(ItemsPage), typeof(ItemsPage));
+            Routing.RegisterRoute(nameof(GetWorkPage), typeof(GetWorkPage));
+            Routing.RegisterRoute(nameof(MapPage), typeof(MapPage));
+
 
             //this.AboutShellContent.Title = new AboutViewModel().Title;
             //this.ItemsShellContent.Title = new ItemsViewModel().Title;
